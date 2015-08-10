105f2605b398a7567d915fa18fa556a830a2976a
alan@igelman.com <Sup3rm1n>
2015-08-10 09:25:49 -0400

    Add sample phantomjs code from http://jonnnnyw.github.io/php-phantomjs/usage.html

--- scratch.php
+++ scratch.php
@@ -5,6 +5,20 @@
  * Date: 8/10/15
  * Time: 8:54 AM
  */
+use JonnyW\PhantomJs\Client;
 
-phpinfo();
+$client = Client::getInstance();
+
+$request  = $client->getMessageFactory()->createRequest();
+$response = $client->getMessageFactory()->createResponse();
+
+$request->setMethod('GET');
+$request->setUrl('http://google.com');
+
+$client->send($request, $response);
+
+if($response->getStatus() === 200) {
+    echo $response->getContent();
+}
+//phpinfo();
 ?>
