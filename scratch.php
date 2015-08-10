0838c9e50b793b22083e8c97adece29b6e66311e
alan@igelman.com <Sup3rm1n>
2015-08-10 11:17:43 -0400

    Get phantomjs working by making files in /bin executable. Add echo of http status to display output in case of non-200 status (which is the case for the sample usage).

--- scratch.php
+++ scratch.php
@@ -5,6 +5,7 @@
  * Date: 8/10/15
  * Time: 8:54 AM
  */
+require './vendor/autoload.php';
 use JonnyW\PhantomJs\Client;
 
 $client = Client::getInstance();
@@ -17,6 +18,7 @@ $request->setUrl('http://google.com');
 
 $client->send($request, $response);
 
+echo $response->getStatus() . PHP_EOL;
 if($response->getStatus() === 200) {
     echo $response->getContent();
 }
